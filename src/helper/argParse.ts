ea8f94f8315d9695db49ef086b6b15715ddb8ab9
Joel <rootjdev@gmail.com>
2022-07-25 17:39:09 +0930

    fix bridge description

--- src/helper/argParse.ts
+++ src/helper/argParse.ts
@@ -24,7 +24,7 @@ export const parsedArgs = yargs
   .option("bridge", {
     alias: "b",
     describe: `The specified method will be searched in this order: ${BRIDGES}. If there is ambiguities, the choice of bridge can be specified. Then, the method is only searched in respective bridge. (Errors out, if not found.)\nIf --bridge gets assigned 'manual', one must specify --cFile and --jsonFile.`,
-    choices: BRIDGES,
+    choices: ["", ...BRIDGES],
     default: "",
     type: "string",
   })
