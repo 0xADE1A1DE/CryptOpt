6a28ad8fc1f8d141be5cf148c870c72eb10b2a73
Joel <rootjdev@gmail.com>
2022-07-25 14:29:24 +0930

    adding fiat-copy hooks

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,7 @@
 // rollup.config.js
 import typescript from "@rollup/plugin-typescript";
+import copy from 'rollup-plugin-copy'
+import shell from 'rollup-plugin-shell'
 
 export default {
   input: "src/CryptOpt.ts",
@@ -7,5 +9,17 @@ export default {
     dir: "dist",
     format: "cjs",
   },
-  plugins: [typescript()],
+  plugins: [
+    typescript(),
+    copy({
+      targets: [
+        {
+          src: ["src/bridge/fiat-bridge/word_by_word_montgomery",
+            "src/bridge/fiat-bridge/unsaturated_solinas"], dest: "./dist"
+        },
+      ]
+    }),
+    shell("cd ./dist && sha256sum unsaturated_solinas word_by_word_montgomery > ./sha256sums")
+
+  ],
 };
