979dd1c82ade39a69f989e3dd417e014ca39d01c
Joel <rootjdev@gmail.com>
2022-07-25 15:07:43 +0930

    co and bar

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,23 +3,27 @@ import typescript from "@rollup/plugin-typescript";
 import copy from 'rollup-plugin-copy'
 import shell from 'rollup-plugin-shell'
 
-export default {
-  input: "src/CryptOpt.ts",
-  output: {
-    dir: "dist",
-    format: "cjs",
-  },
-  plugins: [
-    typescript(),
-    copy({
-      targets: [
-        {
-          src: ["src/bridge/fiat-bridge/word_by_word_montgomery",
-            "src/bridge/fiat-bridge/unsaturated_solinas"], dest: "./dist"
-        },
-      ]
-    }),
-    shell("cd ./dist && sha256sum unsaturated_solinas word_by_word_montgomery > ./sha256sums")
-
-  ],
+const output = {
+  dir: "dist",
+  format: "cjs",
 };
+export default [{
+  input: "src/CryptOpt.ts",
+  output,
+  plugins: [typescript()],
+},
+{
+  input: "src/BetAndRun.ts",
+  output,
+  plugins: [typescript(),
+  copy({
+    targets: [
+      {
+        src: ["src/bridge/fiat-bridge/word_by_word_montgomery",
+          "src/bridge/fiat-bridge/unsaturated_solinas"], dest: "./dist"
+      },
+    ]
+  }),
+  shell("cd ./dist && sha256sum unsaturated_solinas word_by_word_montgomery > ./sha256sums")
+  ],
+}];
