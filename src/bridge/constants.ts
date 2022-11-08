ca271b5ed8e1735629424cf34c90d1a3ec221520
Joel <rootjdev@gmail.com>
2022-11-09 08:53:18 +1030

    add solias to known symbols

--- src/bridge/constants.ts
+++ src/bridge/constants.ts
@@ -23,6 +23,8 @@ export const KNOWN_SYMBOLS: {
   // fiat curves
   fiat_curve25519_carry_mul: { bridge: "fiat", method: "mul", curve: "curve25519" },
   fiat_curve25519_carry_square: { bridge: "fiat", method: "square", curve: "curve25519" },
+  fiat_curve25519_solinas_mul: { bridge: "fiat", method: "mul", curve: "curve25519_solinas" },
+  fiat_curve25519_solinas_square: { bridge: "fiat", method: "square", curve: "curve25519_solinas" },
   fiat_p224_mul: { bridge: "fiat", method: "mul", curve: "p224" },
   fiat_p224_square: { bridge: "fiat", method: "square", curve: "p224" },
   fiat_p256_mul: { bridge: "fiat", method: "mul", curve: "p256" },
