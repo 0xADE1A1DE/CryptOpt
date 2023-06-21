c47fdb4d67881430a07212feef353aa073c342aa
Joel <rootjdev@gmail.com>
2023-06-21 15:00:05 -0400

    remove --asm-stack-size from proof command

--- src/bridge/fiat-bridge/FiatBridge.ts
+++ src/bridge/fiat-bridge/FiatBridge.ts
@@ -17,7 +17,7 @@
 import { accessSync, chmodSync, constants as FS_CONSTANTS, existsSync, mkdirSync, readFileSync } from "fs";
 import { resolve } from "path";
 
-import { datadir, env, preprocessFunction, STACK_OFFSET_IN_ELEMENTS } from "@/helper";
+import { datadir, env, preprocessFunction } from "@/helper";
 import Logger from "@/helper/Logger.class";
 import { sha256Hash } from "@/paul";
 import type { CryptOpt, Fiat } from "@/types";
@@ -203,32 +203,17 @@ export class FiatBridge implements Bridge {
       "--output-asm /dev/null",
     ].join(" ");
 
-    const stackoffsets = readFileSync(hintsFilename)
-      .toString()
-      .split("\n")
-      .map((line) => /mov .*, \[ rsp \+ ([0-9x]+) \].*/.exec(line)?.[1])
-      .filter((match) => typeof match !== undefined)
-      .map(Number)
-      .filter((nu) => !isNaN(nu));
-
-    // those are the 'rsp - ...' and mov [rsp], rbx
-    let stacksize = (STACK_OFFSET_IN_ELEMENTS + 1) * 8;
-    if (stackoffsets.length > 0) {
-      stacksize += Math.max(...stackoffsets);
-    }
-
-    const stack = `--asm-stack-size ${stacksize ?? 0}`;
     const hint = `--hints-file ${hintsFilename}`;
 
     switch (binary) {
       case BINS.dettman:
-        return `${binWithPath}  ${CODE_PROOF_ARGS} '${curve}' '${bitwidth}' '${argwidth}' '${last_limbwidth}' '${prime}' ${required_function} ${hint} ${stack}`;
+        return `${binWithPath}  ${CODE_PROOF_ARGS} '${curve}' '${bitwidth}' '${argwidth}' '${last_limbwidth}' '${prime}' ${required_function} ${hint}`;
       case BINS.unsaturated:
         CODE_PROOF_ARGS += ` --tight-bounds-mul-by 1.000001`;
-        return `${binWithPath}  ${CODE_PROOF_ARGS} '${curve}' '${bitwidth}' '${argwidth}'                     '${prime}' ${required_function} ${hint} ${stack}`;
+        return `${binWithPath}  ${CODE_PROOF_ARGS} '${curve}' '${bitwidth}' '${argwidth}'                     '${prime}' ${required_function} ${hint}`;
       case BINS.wbw_montgomery:
       case BINS.solinas:
-        return `${binWithPath}  ${CODE_PROOF_ARGS} '${curve}' '${bitwidth}'                                   '${prime}' ${required_function} ${hint} ${stack}`;
+        return `${binWithPath}  ${CODE_PROOF_ARGS} '${curve}' '${bitwidth}'                                   '${prime}' ${required_function} ${hint}`;
     }
   }
 
