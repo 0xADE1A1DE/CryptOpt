b39b02a8504f82d716e189d1ecd1e10f4db12a47
Joel <rootjdev@gmail.com>
2023-07-06 09:48:23 +0930

    chore: imports

--- src/optimizer/optimizer.helper.ts
+++ src/optimizer/optimizer.helper.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import os, { cpus } from "os";
+import { hostname, cpus } from "os";
 
 import { CHOICE } from "@/enums";
 import { bl, cy, env, gn, pu, rd, re, shouldProof, SI, yl } from "@/helper";
@@ -152,13 +152,13 @@ export function printStartInfo({
       `\nStart`,
       `on brg/symbolname>>${cy}${bridge}/${symbolname}${re}<<`,
       `>>${cy}${shouldProof({ bridge, proof }) ? "with" : "without"} proofing${re} correct<<`,
-      `on cpu >>${cy}${os.cpus()[0].model}${re}<<`,
+      `on cpu >>${cy}${cpus()[0].model}${re}<<`,
       `writing results to>>${cy}${resultDir}${re}<<`,
       `with seed >>${cy}${seed}${re}<<`,
       `for >>${cy}${SI(evals)}${re}<< evaluations`,
       `against CC>>${cy}${CC} ${CFLAGS}${re}<<`,
       `with cycle goal>>${cy}${cyclegoal}${re}<< for each measurement`,
-      `on host>>${cy}${os.hostname()}${re}<<`,
+      `on host>>${cy}${hostname()}${re}<<`,
       `with pid>>${cy}${process.pid}${re}<<`,
       `using counter>>${cy}${counter}${re}<<`,
       `framePointer=>>${cy}${framePointer}${re}<<`,
