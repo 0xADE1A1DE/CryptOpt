904a3df1a5dcba5022287b56bddb4e8ce6d1d7e8
kanin-kearpimy <kanin.kearpimy@gmail.com>
2024-03-12 13:12:25 +1100

    loop until new partner is different

--- src/model/model.class.ts
+++ src/model/model.class.ts
@@ -283,11 +283,11 @@ export class Model {
       return this.mutatePermutation();
     }
 
-    const partner = min + 1 == max ? min : Paul.chooseBetween(max, min, BIAS.REVERSE_BELL);
-    if (chosen == partner) {
-      // would that ever happen ?
-      return this.mutatePermutation();
+    let partner = min + 1 == max ? min : Paul.chooseBetween(max, min, BIAS.REVERSE_BELL);
+    while (chosen == partner) {
+      partner = Paul.chooseBetween(max, min, BIAS.REVERSE_BELL);
     }
+
     const [a] = Model._order.splice(chosen, 1);
 
     const b = Model._order[partner];
